bee8b51583bb1b5e4d3ed830c27fc9bf7d9c9dcf
ARUNPRASANTH D <61341156+arunprasanthd@users.noreply.github.com>
2021-02-23 10:29:28 +0530

    Added a clickable image feature

--- Image Swapping.py
+++ Image Swapping.py
@@ -7,30 +7,28 @@ import sys
 import os
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QFormLayout, QGroupBox, QVBoxLayout, QMainWindow, QPushButton, QMessageBox, QLabel, QGridLayout, QScrollArea, QGroupBox, QVBoxLayout
+from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QFormLayout, QGroupBox, QMainWindow, QPushButton, QMessageBox, QLabel, QScrollArea
 from PyQt5.QtGui import QPixmap
 
 # To select images folder
 path_ = './Images/'
-path_ = 'C:/ARUNPRASANTH/Ignite 2020/Day 2/Perception Matrix/'
-
-# image_list = os.listdir('./Images')
 image_list = os.listdir(path_)
 current_image = image_list[0]
 back_image_ = image_list[-1]
-# Class to create object for Main Window
 
 
+# Class to create scroll widget
 class Window(QWidget):
     def __init__(self):
         super().__init__()
         formLayout = QFormLayout()
         groupBox = QGroupBox("This Is Group Box")
-        self.labelLis = []
-        self.comboList = []
+        self.labelList = []
+        self.buttonList = []
         self.image_list = image_list
         for i in range(len(image_list)):
-            self.image_ = QLabel('hi')
+            self.image_ = QLabelClickable()
             self.button_ = QPushButton(str(i + 1))
             self.pixmap = QPixmap(path_ + self.image_list[i])
             self.image_.setPixmap(self.pixmap)
@@ -39,9 +37,9 @@ class Window(QWidget):
             self.image_.setFixedWidth(100)
             self.button_.setFixedHeight(30)
             self.button_.setFixedWidth(30)
-            self.labelLis.append(self.image_)
-            self.comboList.append(self.button_)
-            formLayout.addRow(self.comboList[i], self.labelLis[i])
+            self.labelList.append(self.image_)
+            self.buttonList.append(self.button_)
+            formLayout.addRow(self.buttonList[i], self.labelList[i])
         groupBox.setLayout(formLayout)
         scroll = QScrollArea()
         scroll.setWidget(groupBox)
@@ -52,39 +50,44 @@ class Window(QWidget):
         self.show()
 
 
+# Class to create clickable label
+class QLabelClickable(QLabel):
+    clicked = pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super(QLabelClickable, self).__init__(parent)
+
+    def mousePressEvent(self, event):
+        self.ultimo = "Clic"
+
+    def mouseReleaseEvent(self, event):
+        if self.ultimo == "Clic":
+            self.clicked.emit(self.ultimo)
+
+    def performSingleClickAction(self):
+        if self.ultimo == "Clic":
+            self.clicked.emit(self.ultimo)
+
+
+# Class to create object for Main Window
 class InspectionWindow(QMainWindow):
     def __init__(self):
         # To inherit all the attributes and methods from parent class QMainWindow
         super(InspectionWindow, self).__init__()
+
         # To load .ui file into class InspectionWindow
         loadUi('Image Swapping1.ui', self)
+
         self.new_win = Window()
         self.verticalLayout.addWidget(self.new_win)
 
         self.image_list = image_list
         self.current_image = current_image
         self.back_image_ = back_image_
-        self.line_input.setPlaceholderText(
-            'Enter from 1 to {}'.format(len(self.image_list)))
-        # self.grid_.setSpacing(10)
+        self.line_input.setPlaceholderText('Enter from 1 to {}'.format(len(self.image_list)))
         self.button_list = []
         pixmap = QPixmap(path_ + self.image_list[0])
         self.label.setPixmap(pixmap)
-
-        # To dynamically create pushbuttons and insert images in a label
-        # for i in range(len(image_list)):
-        #     button_ = QPushButton(str(i + 1))
-        #     button_.setFixedWidth(30)
-        #     button_.setFixedHeight(30)
-        #     self.button_list.append(button_)
-        #     label_ = QLabel()
-        #     pixmap = QPixmap(path_ + self.image_list[i])
-        #     label_.setPixmap(pixmap)
-        # self.grid_.addWidget(button_, i, 0)
-        # self.grid_.addWidget(label_, i, 1)
-
-        # To set the pushbuttons and images in a grid layout
-        # self.setLayout(self.grid_)
 
         # To perform pushbutton clicking operations to change image
         self.previous_button.clicked.connect(self.previous_image)
@@ -93,21 +96,14 @@ class InspectionWindow(QMainWindow):
         self.back_button.clicked.connect(self.back_image)
 
         # To perform corresponding pushbutton clicking operations to view image
-        for c, d in zip(self.new_win.comboList, self.image_list):
+        for c, d in zip(self.new_win.buttonList, self.image_list):
             c.clicked.connect(lambda xy, d=d: self.click_button(d))
 
-        # To scroll area
-        # groupBox = QGroupBox()
-        # groupBox.setLayout(self.grid_)
-        # scroll = QScrollArea()
-        # scroll.setWidget(groupBox)
-        # scroll.setWidgetResizable(True)
-        # scroll.setFixedHeight(400)
-        # layout = QVBoxLayout(self)
-        # layout.addWidget(scroll)
+        # To perform corresponding thumbnail clicking operations to view image
+        for e, f in zip(self.new_win.labelList, self.image_list):
+            e.clicked.connect(lambda xy, f=f: self.click_button(f))
 
     # Method to view corresponding image of clicked button
-
     def click_button(self, a):
         self.back_image_ = self.current_image
         self.current_image = a
@@ -117,8 +113,7 @@ class InspectionWindow(QMainWindow):
     # Method to view previous image
     def previous_image(self):
         self.back_image_ = self.current_image
-        self.current_image = self.image_list[(self.image_list.index(
-            self.current_image) - 1) % len(self.image_list)]
+        self.current_image = self.image_list[(self.image_list.index(self.current_image) - 1) % len(self.image_list)]
         pixmap = QPixmap(path_ + self.current_image)
         self.label.setPixmap(pixmap)
 
